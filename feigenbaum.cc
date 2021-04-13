107dc9fd7c62853388dd04f4554efffc8a506e78
Matthew Gretton-Dann <matt@gretton-dann.org.uk>
2021-04-13 12:46:28 +0100

    Use FixedPoint for points
    This increases the accuracy of the calculations from 53-bits floating
    point to 64-bit fixed point (Q3.61).

--- feigenbaum.cc
+++ feigenbaum.cc
@@ -12,16 +12,95 @@
  */
 
 #include <SDL.h>
+#include <cassert>
+#include <cstdint>
 #include <iostream>
+
+class FixedPoint {
+public:
+  constexpr FixedPoint(uint64_t units, uint64_t frac)
+      : num_((units << frac_bits_) | frac_bits_) {
+    assert(units < (std::uint64_t(1) << unit_bits_));
+    assert(frac < (std::uint64_t(1) << frac_bits_));
+  }
+
+  FixedPoint(FixedPoint const &) = default;
+  FixedPoint(FixedPoint &&) = default;
+  FixedPoint &operator=(FixedPoint const &) = default;
+  FixedPoint &operator=(FixedPoint &&) = default;
+  ~FixedPoint() = default;
+  static constexpr unsigned unit_bits_ = 3;
+  static constexpr unsigned frac_bits_ = 61;
+
+  constexpr FixedPoint &operator+=(FixedPoint rhs) noexcept {
+    assert((num_ + rhs.num_) > num_);
+    num_ += rhs.num_;
+    return *this;
+  }
+
+  constexpr FixedPoint &operator-=(FixedPoint rhs) noexcept {
+    assert(rhs.num_ < num_);
+    num_ -= rhs.num_;
+    return *this;
+  }
+
+  constexpr FixedPoint &operator/=(uint32_t rhs) noexcept {
+    num_ /= rhs;
+    return *this;
+  }
+
+  constexpr FixedPoint &operator*=(FixedPoint rhs) noexcept {
+    unsigned __int128 res = __int128(num_) * rhs.num_;
+    num_ = res >> frac_bits_;
+    return *this;
+  }
+
+  constexpr uint32_t operator*(uint32_t rhs) noexcept {
+    unsigned __int128 res = __int128(num_) * rhs;
+    return res >> frac_bits_;
+  }
+
+  constexpr bool operator==(FixedPoint rhs) noexcept {
+    return num_ == rhs.num_;
+  }
+
+private:
+  std::uint64_t num_;
+};
+
+constexpr FixedPoint operator+(FixedPoint lhs, FixedPoint rhs) noexcept {
+  lhs += rhs;
+  return lhs;
+}
+
+constexpr FixedPoint operator-(FixedPoint lhs, FixedPoint rhs) noexcept {
+  lhs -= rhs;
+  return lhs;
+}
+
+constexpr FixedPoint operator/(FixedPoint lhs, unsigned rhs) noexcept {
+  lhs /= rhs;
+  return lhs;
+}
+
+constexpr FixedPoint operator*(FixedPoint lhs, FixedPoint rhs) noexcept {
+  lhs *= rhs;
+  return lhs;
+}
+
+static constexpr FixedPoint fp4 = FixedPoint(4, 0);
+static constexpr FixedPoint fp0 = FixedPoint(0, 0);
+static constexpr FixedPoint fp1 = FixedPoint(1, 0);
+static constexpr FixedPoint fp0_5 = fp1 / 2U;
 
 int main(int argc, char **argv) {
   // Range of values for k.
-  double start = 0.0;
-  double end = 4.0;
+  FixedPoint start(fp0);
+  FixedPoint end(fp4);
 
   // How many iterations to do.  We do warmup number of steps to try and get to
   // the steady state and then plot `points` iterations.
-  unsigned warmup = 1000;
+  unsigned warmup = 10000;
   unsigned points = 100;
 
   if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_VIDEO) < 0) {
@@ -50,9 +129,9 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  int width = 0;
-  int height = 0;
-  if (SDL_GetRendererOutputSize(renderer, &width, &height) < 0) {
+  int w = 0;
+  int h = 0;
+  if (SDL_GetRendererOutputSize(renderer, &w, &h) < 0) {
     std::clog << "Window size can not be determined! SDL_Error: "
               << SDL_GetError() << "\n";
     SDL_DestroyRenderer(renderer);
@@ -62,24 +141,29 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  assert(w > 0);
+  uint32_t width(w);
+  assert(h > 0);
+  uint32_t height(h);
+
   // Clear the screen.
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-  double step = (end - start) / width;
-  double k = 0;
+  FixedPoint step = (end - start) / width;
+  FixedPoint k(start);
 
   // For each value of k (i is horizontal position).
   for (unsigned i = 0; i < width; ++i) {
-    double x = 0.5;
+    FixedPoint x(fp0_5);
 
     for (unsigned j = 0; j < warmup; ++j) {
-      x = k * x * (1 - x);
+      x = k * x * (fp1 - x);
     }
 
     for (unsigned j = 0; j < points; ++j) {
-      x = k * x * (1 - x);
+      x = k * x * (fp1 - x);
       SDL_RenderDrawPoint(renderer, i, (height - 1) - x * height);
     }
 
